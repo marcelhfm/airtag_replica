5e733997380b98a12aa3d85cdfb9b132d8ed1547
Marcel Hofmann <hofman24@hm.edu>
2024-06-21 13:58:02 +0200

    feat: renaming

--- main/main.c
+++ main/main.c
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
